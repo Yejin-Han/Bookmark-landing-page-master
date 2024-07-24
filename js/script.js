be37125d5b54c7343cd871563a00c8e41b87a6b5
Yejin Han (한예진) <119913434+Yejin-Han@users.noreply.github.com>
2024-07-24 10:05:18 +0900

    accessibility fix 2

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ headerToggle.addEventListener('click', (e) => {
 
 
 // tab
-const tabItems = document.querySelectorAll('.tab-item');
+const tabItems = document.querySelectorAll('.tab-link');
 const tabPanels = document.querySelectorAll('.tab-panel');
 
 tabItems.forEach(item => {
@@ -44,29 +44,6 @@ questions.forEach(q => {
 });
 
 // contact form validation
-/* document.querySelector(".contact-form").addEventListener("submit", function(event) {
-    var emailInput = document.getElementById("email");
-    var emailValue = emailInput.value;
-    var errorMessage = document.querySelector(".error-msg");
-    var errorIcon = document.querySelector(".error-icon");
-  
-    // Simple email validation regex
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-    if (!emailValue || !emailPattern.test(emailValue)) {
-      event.preventDefault();
-      emailInput.classList.add("error");
-      errorMessage.style.display = "block";
-      errorIcon.style.display = "block";
-      errorMessage.textContent = emailValue ? `Whoops, make sure it's an email` : "Plase enter your email address.";
-    } else {
-      emailInput.classList.remove("error");
-      errorMessage.style.display = "none";
-      errorIcon.style.display = "none";
-      // Submit the form or do other stuff
-    }
-  }); */
-
 const contactInput = document.querySelector('.contact-input');
 contactInput.addEventListener('invalid', function(e) {
     e.preventDefault();
@@ -75,8 +52,6 @@ contactInput.addEventListener('invalid', function(e) {
 const emailValidate = () => {
     const errorMessage = document.querySelector('.error-msg');
     const errorIcon = document.querySelector('.error-icon');
-
-    // contactInput.setCustomValidity('');
     
     if(!contactInput.checkValidity()) {
         contactInput.classList.add('error');
@@ -90,9 +65,3 @@ const emailValidate = () => {
 
     console.log(contactInput.checkValidity());
 }
-
-/* if (emailInput.validity.valueMissing) {
-    errorMessage.textContent = "이메일 주소를 입력해주세요.";
-  } else if (emailInput.validity.typeMismatch) {
-    errorMessage.textContent = "Whoops, make sure it's an email";
-  } */
